318ade670bddc31cd97e542a5b04e3f8e816edb5
seunghyun <ggyool@naver.com>
2025-04-15 12:14:47 +0900

    init project

---
+++ src/main/kotlin/com/ggyool/servlet_ticketing_app/ServletTicketingAppApplication.kt
@@ -0,0 +1,11 @@
+package com.ggyool.servlet_ticketing_app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServletTicketingAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServletTicketingAppApplication>(*args)
+}
