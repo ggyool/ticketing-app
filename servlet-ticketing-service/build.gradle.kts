2be68107a2bf256a8d9c1425968103f79aadadc2
seunghyun <ggyool@naver.com>
2025-04-24 00:23:30 +0900

    [ticketing-service] redis 라이브러리 lettuce에서 redisson 으로 변경

--- servlet-ticketing-service/build.gradle.kts
+++ servlet-ticketing-service/build.gradle.kts
@@ -20,8 +20,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.redisson:redisson-spring-boot-starter:3.44.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
