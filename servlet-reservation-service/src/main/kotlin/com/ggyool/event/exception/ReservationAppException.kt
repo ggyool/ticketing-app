4586e686f7190938ac35c9661a198cffd8018243
seunghyun <ggyool@naver.com>
2025-04-26 19:42:12 +0900

    [reservation-service] 불필요하다고 생각하여 모듈 제거

--- servlet-reservation-service/src/main/kotlin/com/ggyool/event/exception/ReservationAppException.kt
+++
@@ -1,8 +0,0 @@
-package com.ggyool.event.exception
-
-import org.springframework.http.HttpStatus
-
-class ReservationAppException(
-    val httpStatus: HttpStatus,
-    val reason: String,
-) : RuntimeException(reason)
