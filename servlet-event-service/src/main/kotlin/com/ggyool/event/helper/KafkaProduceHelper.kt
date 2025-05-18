35ea3c67ab71968d55eb888aa4aa3d11a99312b8
seunghyun <ggyool@naver.com>
2025-05-18 10:51:32 +0900

    [event-service] ProduceHelper 타입에 따른 직렬화 변경 및 에러 발생시 로깅 추가

--- servlet-event-service/src/main/kotlin/com/ggyool/event/helper/KafkaProduceHelper.kt
+++ servlet-event-service/src/main/kotlin/com/ggyool/event/helper/KafkaProduceHelper.kt
@@ -3,6 +3,8 @@ package com.ggyool.event.helper
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.ggyool.common.event.DeadLetterEvent
 import com.ggyool.event.application.producer.DeadLetterKafkaProducer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
 
 @Suppress("UNCHECKED_CAST")
@@ -18,14 +20,21 @@ private val objectMapper: ObjectMapper by lazy {
     ApplicationContextProvider.getBean(ObjectMapper::class.java)
 }
 
+private val logger: Logger = LoggerFactory.getLogger("KafkaProduceHelper")
+
 fun produceWithDlt(
     topic: String,
     key: String,
     payload: Any,
 ) {
     try {
-        kafkaTemplate.send(topic, key, objectMapper.writeValueAsString(payload))
+        if (payload is String) {
+            kafkaTemplate.send(topic, key, payload)
+        } else {
+            kafkaTemplate.send(topic, key, objectMapper.writeValueAsString(payload))
+        }
     } catch (ex: Exception) {
+        logger.error("[$topic] Producing Error (key: $key, payload: $payload}", ex)
         deadLetterKafkaProducer.send(
             DeadLetterEvent(
                 reason = ex.javaClass.name,
